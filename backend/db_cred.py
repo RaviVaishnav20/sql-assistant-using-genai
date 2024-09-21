3d1a2ce1b078b4812012271eb9c5f36e914a4f2d
Ravi Das Vaishnav <43400594+RaviVaishnav20@users.noreply.github.com>
2024-09-21 14:28:05 +0530

    Update db_cred.py

--- backend/db_cred.py
+++ backend/db_cred.py
@@ -1,7 +1,9 @@
-DB_HOST = 'localhost'
-DB_PORT = '5432'
-DB_NAME = 'hardware_shop'
-DB_USER = 'postgres'
-DB_PASSWORD = 'admin123'
+import os
 
-DATABASE_URL = f'postgresql://postgres:admin123@localhost:5432/hardware_shop'
+DB_HOST = os.getenv('DB_HOST', 'localhost')  # Default to localhost if not set
+DB_PORT = os.getenv('DB_PORT', '5432')
+DB_NAME = os.getenv('DB_NAME', 'hardware_shop')
+DB_USER = os.getenv('DB_USER', 'postgres')
+DB_PASSWORD = os.getenv('DB_PASSWORD', 'admin123')
+
+DATABASE_URL = f'postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}'
